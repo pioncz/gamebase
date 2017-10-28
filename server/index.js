f59e2920076297db1712a94a2e9b547490f2792e
Piotr <pioncz@gmail.com>
2017-10-28 17:11:56 +0200

    Boilerplate

---
+++ server/index.js
@@ -0,0 +1,60 @@
+'use strict';
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+const git = require('git-rev');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+var cors = require('cors');
+
+function handleError(req, res, error) {
+  console.error(error.statusCode, error.error, error.options.uri);
+  res.send(error.statusCode);
+}
+
+git.branch(function(branchName) {
+  var configFile = './../config/develop.json'
+  var config = require(configFile);
+  var baseUrl = config.baseUrl;
+
+  /**
+  * Module variables
+  */
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.hsts());
+  app.use(helmet.noSniff());
+  app.use(helmet.xssFilter());
+
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json({ inflate: false }));
+  app.use(cookieParser());
+  app.use(cors());
+
+  app.use('/ping', function(req, res) {
+    console.log((new Date()).toISOString());
+    res.send(200);
+  });
+
+  app.use('/', express.static('dist'));
+
+  app.use(function (req, res) {
+    var fileName = __dirname + '/../dist/index.html';
+      fs.readFile(fileName, 'utf8', function (err,data) {
+      if (err) {
+        console.log(err);
+        res.status(404).send({error: 'index not found', url: req.url});
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
+  let port = config.server.port;
+  http.Server(app).listen(port, function(){
+    console.log('Listening on *:' + port);
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,98 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
+module.exports = function(env) {
+  if (!env) env = {};
+
+  let isProduction = false;
+  let gitInfo = new GitRevisionPlugin({branch: true, versionCommand: 'describe --always --tags'});
+  let branch = gitInfo.branch();
+  let cfgName = ((branch === 'master' || env.production)?'./config/master.json':'./config/develop.json');
+  const cfg = require(cfgName);
+  cfg.version = {
+    tag: gitInfo.version(),
+    branch: branch,
+    commit: (gitInfo.commithash()).substr(0,6)
+  };
+  const extractSass = new ExtractTextPlugin({
+    filename: "[name].min.css",
+    disable: false
+  });
+
+  return {
+    entry: {
+      webapp: './webapp/index.jsx'
+    },
+    output: {
+      filename: '[name].min.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: isProduction?true:'source-map',
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['env', 'react', 'stage-1']
+          }
+        },
+        {test: /\.html$/, loader: 'html-loader'},
+        { test: /\.js$/, loader: 'imports-loader?THREE=three'},
+        { test: /\.scss$/, use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader",
+            options: {
+              includePaths: [
+                path.resolve(__dirname, 'node_modules'),
+              ],
+              sourceMap: true
+            }
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        }) }
+      ]
+    },
+    resolve: {
+      modules: ['node_modules']
+    },
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new webpack.DefinePlugin({
+        __CONFIG__: JSON.stringify(cfg)
+      }),
+      new HtmlWebpackPlugin({
+        inject: true,
+        chunks: ['webapp'],
+        filename: 'index.html',
+        minify: {
+          collapseWhitespace: isProduction,
+          minifyCSS: isProduction,
+          minifyJS: isProduction,
+          removeComments: isProduction,
+        },
+        template: 'webapp/index.html',
+      }),
+      extractSass,
+      new CopyWebpackPlugin([
+        { from: 'static', to: 'static' },
+      ], {
+        copyUnmodified: true
+      })
+    ],
+    devServer: {
+      port: 5000,
+      host: '0.0.0.0'
+    }
+  };
+}
