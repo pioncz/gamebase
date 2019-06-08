131a5d52b3e76d3023052ca5208e74104e6719e7
roleksy <herillus@gmail.com>
2019-06-08 20:46:25 +0200

    jakaś lipa z importem

---
+++ webapp/components/adminTabs/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {MDCTabBar} from '@material/tab-bar';
+
+const tabBar = new MDCTabBar(document.querySelector('.mdc-tab-bar'));
+
+
+class AdminTabs extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default AdminTabs;
