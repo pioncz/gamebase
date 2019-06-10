387e73ff28aaaee9d21c560e67c933828478374e
roleksy <herillus@gmail.com>
2019-06-10 19:42:36 +0200

    included tabs

--- webapp/pages/admin/index.jsx
+++ webapp/pages/admin/index.jsx
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
 import ReactJson from 'react-json-view'
 import './index.sass'
-import AdminTabs from 'components/adminTabs';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
 
-export default class Admin extends Component {
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+class SimpleTabs  extends Component {
+  state = {
+    value: 0,
+    serverStats: {},
+  };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   constructor(props) {
     super(props);
-    
-    this.state = {
-      serverStats: {},
-    };
   
     this.getStatsInterval = null;
     this.connectorInstance = this.props.connectorInstance;
@@ -34,9 +62,35 @@ export default class Admin extends Component {
     });
   }
   render() {
-    return <div className="admin-page">
-      <div>Server data</div>
-      <ReactJson src={this.state.serverStats} theme="monokai" />
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs value={value} onChange={this.handleChange}>
+          <Tab label="Server data" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>
+        
+      <div className="admin-page">
+        <div>Server data</div>
+        <ReactJson src={this.state.serverStats} theme="monokai" />
+      </div>
+
+      </TabContainer>}
+      {value === 1 && <TabContainer>Item Two</TabContainer>}
+      {value === 2 && <TabContainer>Item Three</TabContainer>}
     </div>
+    );
   }
 }
+
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
