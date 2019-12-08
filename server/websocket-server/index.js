862a018ee85018b72484bf9aec65c3bbbbd543ea
pioncz <pioncz@gmail.com>
2019-12-08 13:00:43 +0100

    totalBots = 40

--- server/websocket-server/index.js
+++ server/websocket-server/index.js
@@ -18,7 +18,7 @@ const _nextId = (() => {
 let RoomQueueTimeout = 1 * 1000;
 let MinPlayers = 4;
 
-const TotalBots = 4;
+const TotalBots = 40;
 const RandomDelays = [900, 1400,];
 const Dices = [
   {id:'dice1', colors: ['#fff', '#000',],},// TODO:dopisac kolory z engine
